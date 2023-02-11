edcb0e4d969bec599d12de4963217965fa138d3f
princechibez <chibezprince@gmail.com>
2023-02-11 05:10:50 -0800

    hotel-api-crud-features

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./src/utils/database");
+const roomRoutes = require("./src/routes/room.route");
+require("dotenv/config");
+
+const app = express();
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+// Register all routes here
+app.get("/", (req, res) => res.send("Welcome to this HOTEL API..."));
+
+// room routes
+app.use("/room", roomRoutes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+// error handler middleware
+app.use((err, req, res, next) => {
+  res
+    .status(err.statusCode || 500)
+    .json({ message: err.message, success: false });
+});
+
+const PORT = process.env.PORT || 8000;
+
+connectDB(() => {
+  app.listen(PORT, () => console.log(`Connected to port ${PORT}`));
+});
